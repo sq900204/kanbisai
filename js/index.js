348c1f8551c4bd3139eb707726b7561a12e5d8dc
yinggeABC <906772533@qq.com>
2016-08-19 23:23:39 +0800

    qrcode and feedback

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ window.onload = function(){
     var oNav = $("#nav");
     var oSection = $(".section");
     var oHeader = $("header");
+    var closeBtn = $(".qrCode").find(".close");
+    var feedback = $(".fixedMenu").children("a");
    var navScroll = new IScroll('#nav',{
        mouseWheel:true,
        scrollbars:true
@@ -29,4 +31,19 @@ window.onload = function(){
         navScroll.refresh();
         sectionScroll.refresh();
     }
+
+    closeBtn.css({
+        cursor:"pointer"
+    });
+
+    closeBtn.on("click",function(){
+        $(this).parent().slideUp(500);
+    });
+
+    feedback.on("mouseover",function(){
+        $(this).addClass("over");
+    })
+    feedback.on("mouseleave",function(){
+        $(this).removeClass("over");
+    })//用增加和移除class的方法改变样式
 }
